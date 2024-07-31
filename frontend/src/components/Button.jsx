c5718f524f2177bb7f4fb39ee06c46752088b2aa
chanr335 <76451685+chanr335@users.noreply.github.com>
2024-07-31 01:12:33 -0400

    More Buttons

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,18 +1,21 @@
 import { useState } from "react";
 
 export default function Button(){
-    const [state, setState] = useState(0)
-    
-    function handleClick(){
-        return(
-            setState(state + 1)
-        );
-    }
+    const [state1, setState1] = useState(0)
+    const [state2, setState2] = useState(0)
+    const [state3, setState3] = useState(0)
+    const [state4, setState4] = useState(0)
 
     return(
         <div>
-            <button className="btn" onClick={handleClick}>Button</button>
-            {state}
+            <button className="btn" onClick={() => setState1(state1 + 1)}>Button</button>
+            {state1}
+            <button className="btn" onClick={() => setState2(state2 + 1)}>Button</button>
+            {state2}
+            <button className="btn" onClick={() => setState3(state3 + 1)}>Button</button>
+            {state3}
+            <button className="btn" onClick={() => setState4(state4 + 1)}>Button</button>
+            {state4}
         </div>
     );
 }
