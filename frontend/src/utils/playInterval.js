2f38bab329d7e667a90f12a263ef67872a1865da
chanr335 <76451685+chanr335@users.noreply.github.com>
2024-08-16 20:12:16 -0400

    Added Descending Logic

--- frontend/src/utils/playInterval.js
+++ frontend/src/utils/playInterval.js
@@ -8,15 +8,27 @@ function playInterval(gamemode, newNoteList, rootNote, intNote) {
     synth.triggerAttackRelease(newNoteList[rootNote][0] + "4", "8n", time);
   }, "+0"); // 0 seconds from now
 
-  if (newNoteList[intNote][1] < newNoteList[rootNote][1]) {
-    console.log(newNoteList[intNote][1] < newNoteList[rootNote][1]);
-    Tone.getTransport().schedule((time) => {
-      synth.triggerAttackRelease(newNoteList[intNote][0] + "5", "8n", time);
-    }, "+1"); // 1 second from now
-  } else {
-    Tone.getTransport().schedule((time) => {
-      synth.triggerAttackRelease(newNoteList[intNote][0] + "4", "8n", time);
-    }, "+1"); // 1 second from now
+  if (gamemode === "Ascending") {
+    if (newNoteList[intNote][1] < newNoteList[rootNote][1]) {
+      console.log(newNoteList[intNote][1] < newNoteList[rootNote][1]);
+      Tone.getTransport().schedule((time) => {
+        synth.triggerAttackRelease(newNoteList[intNote][0] + "5", "8n", time);
+      }, "+1"); // 1 second from now
+    } else {
+      Tone.getTransport().schedule((time) => {
+        synth.triggerAttackRelease(newNoteList[intNote][0] + "4", "8n", time);
+      }, "+1"); // 1 second from now
+    }
+  } else if (gamemode === "Descending") {
+    if (newNoteList[intNote][1] > newNoteList[rootNote][1]) {
+      Tone.getTransport().schedule((time) => {
+        synth.triggerAttackRelease(newNoteList[intNote][0] + "3", "8n", time);
+      }, "+1"); // 1 second from now
+    } else {
+      Tone.getTransport().schedule((time) => {
+        synth.triggerAttackRelease(newNoteList[intNote][0] + "4", "8n", time);
+      }, "+1"); // 1 second from now
+    }
   }
 }
 
