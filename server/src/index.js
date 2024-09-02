acb59be4fd42fdcf29571e945aa171de8c66930b
chanr335 <76451685+chanr335@users.noreply.github.com>
2024-09-01 21:53:51 -0400

    Firebase Auth Setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,19 @@
 import app from "./app.js";
 import router from "./routes/navRoutes.js";
+import admin from "firebase-admin";
+import credentials from "../firebaseAccountKey.json" assert { type: "json" };
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.use("/api", router)
+// const admin = require("firebase-admin");
+// const credentials = require("../firebaseAccountKey.json");
 
-
-app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`)
+admin.initializeApp({
+  credential: admin.credential.cert(credentials),
 });
 
+app.use("/api", router);
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running on PORT ${port}.`);
+});
